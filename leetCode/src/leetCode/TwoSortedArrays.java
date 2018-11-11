8ef354a51d9c8664e8481d1920b6a4ad968b10ad
smritijain <smritijain@smritis-air.fios-router.home>
2018-11-10 22:41:26 -0500

    Adding TwoSortedArray class

---
+++ leetCode/src/leetCode/TwoSortedArrays.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package leetCode;
+
+/**
+ * @author smritijain
+ *
+ */
+public class TwoSortedArrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
